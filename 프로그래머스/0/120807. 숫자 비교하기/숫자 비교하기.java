3cfa021132bb521f3f01bb3f50c93bd8200fe9bc
Seungyoon <117635177+hs-2171395-seungyoonkim@users.noreply.github.com>
2026-07-29 21:10:10 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int a = 0;
+        if(num1 == num2){
+            a = 1;
+        }
+        else {
+            a = -1;
+        }
+        return a;
+    }
+}
